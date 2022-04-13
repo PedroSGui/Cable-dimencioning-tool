08c63a50bcfcfa266a5742b0dca1fe3505c01ba5
pedro henrique <pedro.souzagui021@gmail.com>
2022-04-13 14:57:48 +0100

    added database

--- db.py
+++ db.py
@@ -1,5 +1,9 @@
 
 import sqlite3
+import math
+import random
+import copy
+import time
 
 from sqlite3 import Error
 
@@ -49,8 +53,9 @@ create_users = """
 INSERT INTO
   cabos (material, section, perfil, conductors)
 VALUES
+  (1, 90, 1, 1),
   (0, 40, 1, 1),
-  (1, 45, 2, 3);
+  (1, 45, 1, 3);
 """
 
 execute_query(connection, create_users)
@@ -70,29 +75,53 @@ def execute_read_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
-
-'''
-for cabos in cabos:
-    print(cabos)
-'''
-
-
-
 # Começa a partir daqui
 
 
 menu_options = {
-    1: 'Adicionar itens',
+    1: 'Exit',
     2: 'Ver tabela',
     3: 'Calculo de secção',
-    4: 'Exit',
+    4: 'Adicionar itens',
 }
+
+class cabo:
+    def __init__(self): 
+        self.U = int(input('Qual o nivel de tensao: '))
+        self.Scc = int(input('Qual a potencia de cc: '))
+        self.S = int(input('Qual a potencia nominal: '))
+        self.Perf = int(input('Qual o perfil: '))
+        self.Disp = int(input('Qual a disposicao: '))
+        self.Cond = int(input('Quantos condutores: '))
+        self.Mat = int(input('Qual o material (0 - Cu,  1 - Al, 2 - Cu pintado, 3 - Al pintado): '))
+        self.D = int(input('Qual a distãncia: '))
+        self.Is = self.S / ( 1.732 * self.U )
+
+
+def fator_ar(iz, delta1, delta2):
+    return iz * math.sqrt(delta1/delta2) 
+
+def fator_temp(iz, teta1, teta2):
+    return iz * math.sqrt(teta1/teta2)
+
+def fator_alt(iz, h):
+    if h<1000:
+        iz = iz * 1
+    elif h>=1000 & h<2000:
+        iz = iz * 1
+    elif h>=2000 & h<3000:
+        iz = iz * 0.99
+    elif h>=3000 & h<4000:
+        iz = iz * 0.96    
+    else:
+        iz = iz * 0.9
+    return iz
 
 def print_menu():
     for key in menu_options.keys():
         print (key, '--', menu_options[key] )
 
-def option1():
+def option4():
     material = input('Qual o material (0 pra cobre e 1 pra aluminio): ')
     section = input('Qual a secção: ')
     perfil = input('Qual o perfil: ')
@@ -111,7 +140,37 @@ def option2():
     print("\n\n")
 
 def option3():
-    f=1
+    # Entrada de dados
+    meu_cabo  = cabo() 
+    # Fim da entrada de dados
+
+    # Fase 2 - bersão base - Contas regime permanente
+    delta1=1
+    delta2=1
+    teta1=1
+    teta2=1
+    h=0
+    permanente = int(input('Tem condições diferentes da padrao (1 se sim e 0 se não): '))
+    if permanente == 1:
+        h =int(input('Qual a altitude: ')) # colocar outras correções
+    
+    
+    select_cabos = "SELECT * FROM cabos WHERE perfil = "+ str(meu_cabo.Perf) +" AND material = "+ str(meu_cabo.Mat) +""
+    cabos = execute_read_query(connection, select_cabos)
+    for cabos in cabos:
+        print(cabos)
+    print(select_cabos)
+    print("\n\n")
+    for i in cabos:
+        print(i)
+    print("\n\n")
+    iz = 100
+
+    iz = fator_ar(iz, delta1, delta2)
+    iz = fator_temp(iz, teta1, teta2)
+    iz = fator_alt(iz, h)
+
+    # Fim Contas regime permanente
 
 
 if __name__=='__main__':
@@ -124,14 +183,14 @@ if __name__=='__main__':
             print('Wrong input. Please enter a number ...')
         #Check what choice was entered and act accordingly
         if option == 1:
-           option1()
+            print('Thanks message before exiting')
+            exit()
         elif option == 2:
             option2()
         elif option == 3:
             option3()
         elif option == 4:
-            print('Thanks message before exiting')
-            exit()
+            option4()
         else:
             print('Invalid option. Please enter a number between 1 and 4.')
 
--- main.py
+++ main.py
@@ -2,13 +2,84 @@ import random
 import copy
 import time
 import sqlite3
+import math
 
 from sqlite3 import Error
 
 class cabo:
     def __init__(self): 
-        f=1
+        self.U = int(input('Qual o nivel de tensao: '))
+        self.Scc = int(input('Qual a potencia de cc: '))
+        self.S = int(input('Qual a potencia nominal: '))
+        self.Perf = int(input('Qual o perfil: '))
+        self.Disp = int(input('Qual a disposicao: '))
+        self.Cond = int(input('Quantos condutores: '))
+        self.Mat = int(input('Qual o material (0 - Cu,  1 - Al, 2 - Cu pintado, 3 - Al pintado): '))
+        self.D = int(input('Qual a distãncia: '))
+        self.Is = self.S / ( 1.732 * self.U )
+
+
+def fator_ar(iz, delta1, delta2):
+    return iz * math.sqrt(delta1/delta2) 
+
+def fator_temp(iz, teta1, teta2):
+    return iz * math.sqrt(teta1/teta2)
+
+def fator_alt(iz, h):
+    if h<1000:
+        iz = iz * 1
+    elif h>=1000 & h<2000:
+        iz = iz * 1
+    elif h>=2000 & h<3000:
+        iz = iz * 0.99
+    elif h>=3000 & h<4000:
+        iz = iz * 0.96    
+    else:
+        iz = iz * 0.9
+    return iz
 
 if __name__=='__main__':
-    f=1
+
+    # Entrada de dados
+    meu_cabo  = cabo() 
+    # Fim da entrada de dados
+
+    # Fase 2 - bersão base - Contas regime permanente
+    delta1=1
+    delta2=1
+    teta1=1
+    teta2=1
+    h=0
+    permanente = int(input('Tem condições diferentes da padrao (1 se sim e 0 se não): '))
+    if permanente == 1:
+        h =int(input('Qual a altitude: ')) # colocar outras correções
+    
+
+        
+
+    iz = 100
+
+    iz = fator_ar(iz, delta1, delta2)
+    iz = fator_temp(iz, teta1, teta2)
+    iz = fator_alt(iz, h)
+
+    # Fim Contas regime permanente
+
+    '''
+    if meu_cabo.Mat == 0 or 2:
+        carga_res_flex = 1000 # 1000 a 1200
+    else:
+        carga_res_flex = 400 # 400 a 600  
+    
+    
+
+    icc = meu_cabo.Scc / (math.sqrt(3) * meu_cabo.U)
+    X = 1.8
+    Ich = 0.93 * Ich3F = 0.93 * X * math.sqrt(2) * meu_cabo.icc
+    F = 2.04 * (10**-2) * Ich*Ich * (l/a)
+
+    mf = F * l/16
+    W = mf/carga_res_flex
+    '''
+    
 
