b8362defa63882ec812d3f98980f31dd2a11ed0d
pedro henrique <pedro.souzagui021@gmail.com>
2022-05-12 16:20:31 +0100

    com defeitos

--- main.py
+++ main.py
@@ -87,13 +87,16 @@ class cabolist:
         self.peso = peso
         self.inercia = inercia
         self.w = w
+        self.F=0
         
-        if (self.material == 0) | (self.material == 2):
+        if (self.material == 0) | (self.material == 1):
             self.custo = peso*4.20 #(peso/km)*preçoCU
-        if (self.material == 1) | (self.material == 3):
+            self.alfa = 0.000017 #IMPORTANTE COLOCAR VALOR CERTO
+        if (self.material == 2) | (self.material == 3):
             self.custo = peso*2.75 #(peso/km)*preçoAL
+            self.alfa = 0.000017 #IMPORTANTE COLOCAR VALOR CERTO
         
-        E = 1.2 * 1000000
+        self.E = 1.2 * 1000000
         L = meu_cabo.l
         self.fo = 112 * math.sqrt((E*self.inercia)/(self.peso*L*L*L*L))
         
@@ -108,7 +111,11 @@ def show_cable(cable):
     if cable.material == 0:
         print("Material = Cobre não pintado")
     elif cable.material == 1:
+        print("Material = Cobre pintado")
+    elif cable.material == 2:
         print("Material = Aluminio não pintado")
+    elif cable.material == 3:
+        print("Material = Aluminio pintado")
 
     print("Section = ", cable.section)
     print("Perfil = ", cable.perfil)
@@ -121,13 +128,11 @@ def show_cable(cable):
     # Fim da Apresentação de resultado
 
 class cabo:
-    def __init__(self, U, Scc, S, Perf, Disp, Cond, Mat, a, t_cc, l, sigma): 
-
+    def __init__(self, U, Scc, S, Perf, Cond, Mat, a, t_cc, l, sigma): 
         self.U = U
         self.Scc = Scc 
         self.S = S
         self.Perf = Perf
-        self.Disp = Disp
         self.Cond = Cond
         self.Mat = Mat
         self.a = a
@@ -140,8 +145,16 @@ class cabo:
         self.Icc = self.Scc / ( 1.732 * self.U )
         self.ich = self.X * 1.414 * self.Icc
         self.fe=2.04*0.01*self.ich*self.ich*self.l/self.a
+        self.mf = (self.fe* self.l)/16
+        self.Ith = 0
+        self.m = 0
+        self.n = 0
+        self.varTem=0
+        #self.kLinha = e/(u.C)   #faltando
+        
 
-meu_cabo = cabo(1,1,1,3,1,1,0,1,1,1,1)
+#meu_cabo = cabo(1,1,1,3,1,0,1,1,1,1)
+meu_cabo = cabo(15000,500000000,1250000,5,1,1,35,0.5,180,12000)
 flag = 1 #IMPORTANTE MUDAR DEPOIS
 
 def fator_ar(iz, delta1, delta2):
@@ -173,15 +186,34 @@ def custo():
     show_cable(chepest)
 
 def ressonancia():
-    for i in range(len(db_cabo_list)):
-        if (db_cabo_list[i].fo>45 and db_cabo_list[i].fo<55) or (db_cabo_list[i].fo>90 and db_cabo_list[i].fo<110):
-                db_cabo_list.remove(db_cabo_list[i])
+    #print(db_cabo_list[0].id)
+    for elem in db_cabo_list:
+        #print("$$", db_cabo_list[i].fo)
+        #show_cable(db_cabo_list[i])
+        if (elem.fo>45 and elem.fo<55) or (elem.fo>90 and elem.fo<110):
+            db_cabo_list.remove(elem)
 
     # the one with less section
     smallest = min (db_cabo_list, key=lambda cabolist: cabolist.section)
 
     #Apresentação de resultado
     print("\n\nRESSONANCIA:") 
+    show_cable(smallest)
+
+def flexao():
+
+    #print(db_cabo_list[0].id)
+    for elem in db_cabo_list:
+        if (elem.w < ((meu_cabo.mf)/meu_cabo.sigma)):
+            db_cabo_list.remove(elem)
+
+    
+
+    # the one with less section
+    smallest = min (db_cabo_list, key=lambda cabolist: cabolist.section)
+
+    #Apresentação de resultado
+    print("\n\nFLEXAO:") 
     show_cable(smallest)
 
 
@@ -228,7 +260,7 @@ def permanente():
         temp = cabolist(id, material, section, perfil, conductor, maxcurrent, tabela, peso, inercia, w)
         #show_cable(temp)
         if temp.maxcurrent > meu_cabo.Is:
-            
+            #print("## cabo:",temp.maxcurrent, "\t caso:",meu_cabo.Is)
             db_cabo_list.append(temp)
 
     # the one with less section
@@ -250,10 +282,11 @@ def option5():
     Perf = int(input('Qual o perfil: '))
     Cond = int(input('Quantos condutores: '))
     Mat = int(input('Qual o material (0 - Cu,  1 - Al, 2 - Cu pintado, 3 - Al pintado): '))
-    t_cc = int(input('Qual o tempo do cc: '))
-    l = int(input('Qual o comprimento do vao: '))
+    t_cc = float(input('Qual o tempo do cc: '))
+    a = float(input('Qual a distancia entre fases: '))
+    l = float(input('Qual o comprimento do vao: '))
     sigma = int(input('Qual a carga de seguranca a flexão: '))
-    temp  = cabo(U, Scc, S, Perf, Disp, Cond, Mat, a, t_cc, l, sigma) 
+    temp  = cabo(U, Scc, S, Perf, Cond, Mat, a, t_cc, l, sigma) 
     # Fim da entrada de dados
 
     # Fase 2 - bersão base - Contas regime permanente
@@ -275,12 +308,10 @@ def option5():
 
 def cc():
     #Condição de CC
-
-    print("\n\n")
-    if (meu_cabo.Mat == 0) | (meu_cabo.Mat == 2):
+    if (meu_cabo.Mat == 0) | (meu_cabo.Mat == 1):
         #Cobre
         k_linha = 148
-    elif (meu_cabo.Mat == 1) | (meu_cabo.Mat == 3):
+    elif (meu_cabo.Mat == 2) | (meu_cabo.Mat == 3):
         #Aluminio
         k_linha = 76
     else:
@@ -288,93 +319,93 @@ def cc():
         return 0
     
     # PRECISO DE AJUDA
-    if meu_cabo.t_cc >0 and meu_cabo.t_cc< 0.015: 
+    if meu_cabo.t_cc >=0 and meu_cabo.t_cc< 0.015: 
         n=1
         m=1.5
-    elif meu_cabo.t_cc >0.015 and meu_cabo.t_cc< 0.02:
+    elif meu_cabo.t_cc >=0.015 and meu_cabo.t_cc< 0.02:
         n=0.96
         m=1.4
-    elif meu_cabo.t_cc >0.02 and meu_cabo.t_cc< 0.025:
+    elif meu_cabo.t_cc >=0.02 and meu_cabo.t_cc< 0.025:
         n=0.98
         m=1.38
-    elif meu_cabo.t_cc >0.025 and meu_cabo.t_cc< 0.03:
+    elif meu_cabo.t_cc >=0.025 and meu_cabo.t_cc< 0.03:
         m=1.2
         n=0.98
-    elif meu_cabo.t_cc >0.03 and meu_cabo.t_cc< 0.035:
+    elif meu_cabo.t_cc >=0.03 and meu_cabo.t_cc< 0.035:
         n=0.97
         m=1.10
-    elif meu_cabo.t_cc >0.035 and meu_cabo.t_cc< 0.04:
+    elif meu_cabo.t_cc >=0.035 and meu_cabo.t_cc< 0.04:
         m=1.10
         n=0.97
-    elif meu_cabo.t_cc>0.04 and meu_cabo.t_cc<0.045:
+    elif meu_cabo.t_cc>=0.04 and meu_cabo.t_cc<0.045:
         m=0.98
         n=0.97
-    elif meu_cabo.t_cc>0.045 and meu_cabo.t_cc<0.05:
+    elif meu_cabo.t_cc>=0.045 and meu_cabo.t_cc<0.05:
         m=0.90
         n=0.97
-    elif meu_cabo.t_cc>0.05 and meu_cabo.t_cc<0.055:
+    elif meu_cabo.t_cc>=0.05 and meu_cabo.t_cc<0.055:
         m=0.8
         n=0.95
-    elif meu_cabo.t_cc>0.055 and meu_cabo.t_cc<0.06:
+    elif meu_cabo.t_cc>=0.055 and meu_cabo.t_cc<0.06:
         m=0.77
         n=0.93
-    elif meu_cabo.t_cc>0.06 and meu_cabo.t_cc<0.065:
+    elif meu_cabo.t_cc>=0.06 and meu_cabo.t_cc<0.065:
         m=0.77
         n=0.93
-    elif meu_cabo.t_cc>0.065 and meu_cabo.t_cc<0.07:
+    elif meu_cabo.t_cc>=0.065 and meu_cabo.t_cc<0.07:
         m=0.7
         n=0.93
-    elif meu_cabo.t_cc>0.07 and meu_cabo.t_cc<0.075:
+    elif meu_cabo.t_cc>=0.07 and meu_cabo.t_cc<0.075:
         m=0.63
         n=0.93
-    elif meu_cabo.t_cc>0.075 and meu_cabo.t_cc<0.08:
+    elif meu_cabo.t_cc>=0.075 and meu_cabo.t_cc<0.08:
         m=0.61
         n=0.93
-    elif meu_cabo.t_cc>0.085 and meu_cabo.t_cc<0.09:
+    elif meu_cabo.t_cc>=0.085 and meu_cabo.t_cc<0.09:
         m=0.6
         n=0.93
-    elif meu_cabo.t_cc>0.09 and meu_cabo.t_cc<0.1:	
+    elif meu_cabo.t_cc>=0.09 and meu_cabo.t_cc<0.1:	
         m=0.6
         n=0.93
-    elif meu_cabo.t_cc>0.1 and meu_cabo.t_cc<0.2:
+    elif meu_cabo.t_cc>=0.1 and meu_cabo.t_cc<0.2:
         m=0.4
         n=0.93
-    elif meu_cabo.t_cc>0.2 and meu_cabo.t_cc<0.3:
+    elif meu_cabo.t_cc>=0.2 and meu_cabo.t_cc<0.3:
         m=0.3
         n=0.92
-    elif meu_cabo.t_cc>0.3 and meu_cabo.t_cc<0.4:
+    elif meu_cabo.t_cc>=0.3 and meu_cabo.t_cc<0.4:
         m=0.18
         n=0.9
-    elif meu_cabo.t_cc>0.4 and meu_cabo.t_cc<0.5:
+    elif meu_cabo.t_cc>=0.4 and meu_cabo.t_cc<0.5:
         m=0.15
         n=0.88
-    elif meu_cabo.t_cc>0.5 and meu_cabo.t_cc<0.6:
+    elif meu_cabo.t_cc>=0.5 and meu_cabo.t_cc<0.6:
         m=0.10
         n=0.87
-    elif meu_cabo.t_cc>0.6 and meu_cabo.t_cc<0.7:
+    elif meu_cabo.t_cc>=0.6 and meu_cabo.t_cc<0.7:
         m=0.08
         n=0.86
-    elif meu_cabo.t_cc>0.7 and meu_cabo.t_cc<0.8:
+    elif meu_cabo.t_cc>=0.7 and meu_cabo.t_cc<0.8:
         m=0.07
         n=0.85
-    elif meu_cabo.t_cc>0.8 and meu_cabo.t_cc<0.9:
+    elif meu_cabo.t_cc>=0.8 and meu_cabo.t_cc<0.9:
         m=0.05
         n=0.84
-    elif meu_cabo.t_cc>0.9 and meu_cabo.t_cc<1.0:
+    elif meu_cabo.t_cc>=0.9 and meu_cabo.t_cc<1.0:
         m=0
         n=0.83
     else:
         m=0
         n=0.6
-
     
-
-    Ith = meu_cabo.Icc * math.sqrt(m+n)
-
-    Sec_min_cc = Ith*math.sqrt(meu_cabo.t_cc)/k_linha
-    for i in range(len(db_cabo_list)):
-        if db_cabo_list[i].section < Sec_min_cc:
-            db_cabo_list.remove(db_cabo_list[i])
+    meu_cabo.m = m
+    meu_cabo.m = n
+    meu_cabo.Ith = meu_cabo.Icc * math.sqrt(meu_cabo.m+meu_cabo.n)
+    Sec_min_cc =  meu_cabo.Ith*math.sqrt(meu_cabo.t_cc)/148
+    for elem in db_cabo_list:
+        #print("## cabo:",db_cabo_list[i].section, "\t caso:",Sec_min_cc)
+        if elem.section < Sec_min_cc:
+            db_cabo_list.remove(elem)
     
     print("CONDIÇÃO DE CC: ")
     smallest = min (db_cabo_list, key=lambda cabolist: cabolist.section)
@@ -382,6 +413,13 @@ def cc():
 
     # pra cada valor de db_cabo_list precisa ver se é maior do que a secção
 
+def esfTer():
+    meu_cabo.varTem = (meu_cabo.klinha * (meu_cabo.Ith/meu_cabo.S)*(meu_cabo.Ith/meu_cabo.S)*meu_cabo.t_cc) + 45
+    for elem in db_cabo_list:
+        elem.F = elem.section * elem.E * elem.alfa * meu_cabo.varTem
+        elem.Fk = (3.14159*3.14159*elem.E*elem.inercia/(elem.Io*elem.Io))
+        if elem.F < elem.Fk:
+            f=1 #qual a força pros apoios
 
 
 if __name__=='__main__':
@@ -409,6 +447,7 @@ if __name__=='__main__':
         elif option == 6:
             permanente()
             cc()
+            flexao()
             ressonancia()
             custo()
         else:
